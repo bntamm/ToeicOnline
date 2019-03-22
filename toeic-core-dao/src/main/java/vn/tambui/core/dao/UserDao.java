46809907634367e1dbf206973ecc79063a9c1453
tambui <bntamm@gmail.com>
2019-03-22 21:40:50 +0700

    create function API for login page to get data from UserEntity

--- toeic-core-dao/src/main/java/vn/tambui/core/dao/UserDao.java
+++ toeic-core-dao/src/main/java/vn/tambui/core/dao/UserDao.java
@@ -4,4 +4,7 @@ import vn.tambui.core.data.dao.GenericDao;
 import vn.tambui.core.persistence.entity.UserEntity;
 
 public interface UserDao extends GenericDao<Integer, UserEntity> {
+
+     UserEntity isUserExist(String name, String password);
+     UserEntity findRoleByUser(String name, String password);
 }
--- toeic-core-test/src/test/java/vn/tambui/core/test/LoginTest.java
+++ toeic-core-test/src/test/java/vn/tambui/core/test/LoginTest.java
@@ -1,4 +1,36 @@
 package vn.tambui.core.test;
 
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+import vn.tambui.core.dao.UserDao;
+import vn.tambui.core.daoimpl.UserDaoImpl;
+import vn.tambui.core.persistence.entity.UserEntity;
+
 public class LoginTest {
+
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    @Test
+    public void checkIsUserExist(){
+        String name = "tambui";
+        String password = "301194";
+
+        UserDao userDao = new UserDaoImpl();
+        UserEntity entity =  userDao.isUserExist(name,password);
+        if(entity != null){
+            log.error("login success");
+        }else{
+            log.error("login failed");
+        }
+    }
+
+    @Test
+    public void checkFindRoleByUser(){
+        String name = "tambui";
+        String password = "301194";
+
+        UserDao userDao = new UserDaoImpl();
+        UserEntity entity =  userDao.findRoleByUser(name,password);
+        log.error(entity.getRoleEntity().getRoleId() +" - "+entity.getRoleEntity().getName());
+    }
 }
